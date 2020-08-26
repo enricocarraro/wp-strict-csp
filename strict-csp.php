80400c6db8bb15d2a26b0df1b4413de236040fce
Enrico Carraro <enricocarraro@google.com>
2020-08-26 15:49:59 +0200

    Corrected issues signaled by Jakub

--- strict-csp.php
+++ strict-csp.php
@@ -17,49 +17,46 @@
  * Author URI:  https://opensource.google.com/
  * License:     GNU General Public License v2 (or later  )
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
- * Text Domain: feature-policy
  */
 
+ // Inject nonces only in the admin area
 if ( is_admin() ) {
     add_filter( 'inline_script_attributes', function ( $attr ) {
         if ( strpos( $attr, ' nonce="' ) === false ) {
-            return 'nonce="' . get_nonce() . '" ' . $attr;
+            $attr .= sprintf( ' nonce="%s"', esc_attr( get_nonce() ) );
         }
         return $attr;
     } );
     add_filter( 'script_loader_tag', 'add_csp_nonce', 10, 2 );
 }
 
-// This action adds Strict CSP to the admin area
+// This action adds CSP headers to the admin area responses
 add_action( 
     'admin_init',
     function () {
-        if ( wp_doing_ajax() || !empty( $_POST ) ) {
-            return;
+        if ( ! wp_doing_ajax() ) {
+            header( "Content-Security-Policy-Report-Only: script-src 'self' 'nonce-" . esc_attr( get_nonce() ) . "' 'unsafe-eval' 'strict-dynamic' https: http:;" );
         }
-        header( "Content-Security-Policy-Report-Only: script-src 'self' 'nonce-" . get_nonce() . "' 'unsafe-eval' 'strict-dynamic' https: http:;" );
     }
- );
+);
 
+// Adds nonces to script tags that don't have one.
 function add_csp_nonce( $tag, $handle )
 {
-    $nonce = get_nonce();
-    if ( strpos( $tag, ' nonce="' ) === false ) {
-        $tag = str_replace( '<script ', '<script nonce="' . $nonce . '" ', $tag );
-    } else if ( substr_count( $tag, ' nonce="' ) !== substr_count( $tag, '<script' ) ) {
-        $tag = str_replace( ' nonce="' . $nonce . '"', '', $tag );
-        $tag = str_replace( '<script ', '<script nonce="' . $nonce . '" ', $tag );
-    }
-    return $tag;
+    $nonce = esc_attr( get_nonce() );
+    $pattern = '/<script\b(?![^>]*\b' . $nonce . '\b)[^>]*/si';
+    $replacement = sprintf('${0} nonce="%s"', $nonce );
+    return preg_replace( $pattern, $replacement, $tag );
 }
 
+
+// Generates a secure hash and sets 'csp_nonce' in the $GLOBALS associative array
 function get_nonce()
 {
     if ( !isset( $GLOBALS['csp_nonce'] ) ) {
         require_once( ABSPATH . 'wp-includes/class-phpass.php' );
-        $hasher = new PasswordHash( 8, false );
-        $nonce = md5( $hasher->get_random_bytes( 100, false ) );
-        $GLOBALS['csp_nonce'] = esc_attr( $nonce  );
+        $hasher = new PasswordHash( 8, /* portable_hashes */ false );
+        $GLOBALS['csp_nonce'] = md5( $hasher->get_random_bytes( 100 ) );
     }
     return $GLOBALS['csp_nonce'];
 }
